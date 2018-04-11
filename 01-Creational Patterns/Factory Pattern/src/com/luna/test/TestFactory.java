d3eb4af558063718e5119ea6773fd701efcabf03
hehanbo <qixingchen@lovelive.moe>
2018-04-11 23:43:57 +0800

    add&fix test

--- 01-Creational Patterns/Factory Pattern/src/com/luna/test/TestFactory.java
+++ 01-Creational Patterns/Factory Pattern/src/com/luna/test/TestFactory.java
@@ -3,14 +3,43 @@ package com.luna.test;
 import com.luna.factory.FruitStore;
 import com.luna.factory.impl.FruitFactory;
 import com.luna.model.Fruit;
+import com.luna.model.impl.Apple;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * 测试水果工厂
  */
+
 public class TestFactory {
-    public static void main(String args[]) throws Exception {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void main() throws Exception {
         FruitStore fruitFactory = new FruitFactory();
         Fruit fruit = fruitFactory.createFruit("Apple");
+        assertTrue(fruit instanceof Apple);
         fruit.show();
+        assertEquals("I am an apple", outContent.toString().replaceAll("\\p{C}", ""));
+        outContent.reset();
     }
 }
